dd35e864651a58ba98501ef1ea033d48303bf40f
Elsa Brown <elsambrown@gmail.com>
2021-04-07 08:33:03 -0400

    models starting point built

--- subscription_app/customers/models.py
+++ subscription_app/customers/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address_1 = models.CharField(max_length=100)
+    address_2 = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=2)
+    postal_code: models.IntegerField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    # def post(self, request)
+    #     return self.create(request)
+
+class Subscription(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True)
+    plan_name = models.CharField(max_length=100, null=True)
+    price = models.IntegerField(default=0, null=True)
+
+    def __str__(self):
+        return self.plan_name
+
+class Gift(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    plan_name = models.CharField(max_length=100)
+    price: models.IntegerField()
+    recipient_email = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.recipient_email
